b4574c498f1bcb76913bbcace9a53104dfa703bb
FanPengC <118272547+FanPengC@users.noreply.github.com>
2023-03-16 17:54:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '范鹏程',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oQcPd6qT5xOVnLFfsJS7lHfRBPr0',
+      id: 'oQcPd6qT5xOVnLFfsJS7lHfRBPr0,oQcPd6ulP46R4VRx4tJ5YLzK2rDo',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'ESHt_I2rOp0TSYuKOmgP4BlDAB2KoltzH9_NZQ_e62o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
